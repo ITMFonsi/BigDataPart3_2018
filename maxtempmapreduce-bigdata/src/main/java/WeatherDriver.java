ed0c20ab18f30654d4951feb05170c85c3f6f11c
tscholy <cts6638@students.fhv.at>
2018-12-18 18:32:19 +0100

    first push

---
+++ maxtempmapreduce-bigdata/src/main/java/WeatherDriver.java
@@ -0,0 +1,34 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WeatherDriver extends Configured implements Tool {
+
+    public int run(String[] strings) throws Exception {
+        JobConf config = new JobConf(getConf(), WeatherDriver.class);
+        config.setOutputKeyClass(Text.class);
+        config.setOutputValueClass(IntWritable.class);
+        config.setMapperClass(WeatherMapper.class);
+        config.setReducerClass(WeatherReducer.class);
+
+        FileInputFormat.addInputPath(config, new Path(strings[0]));
+        FileOutputFormat.setOutputPath(config, new Path(strings[1]));
+
+        Job job = Job.getInstance(config, "Max Temp");
+
+        return job.waitForCompletion(true) ? 0 : 1;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WeatherDriver(), args);
+        System.exit(exitCode);
+    }
+}
---
+++ maxtempmapreduce-bigdata/src/main/java/WeatherReducer.java
@@ -0,0 +1,26 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class WeatherReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text text, Iterator<IntWritable> iterator, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
+        int maxTemperature= 0;
+
+        while(iterator.hasNext()) {
+            int curr = iterator.next().get();
+            if(maxTemperature < curr) {
+                maxTemperature = curr;
+            }
+        }
+        //the temperature in the data-set is represented in celsius times 10
+        maxTemperature = maxTemperature /10;
+        outputCollector.collect(text, new IntWritable(maxTemperature));
+    }
+}

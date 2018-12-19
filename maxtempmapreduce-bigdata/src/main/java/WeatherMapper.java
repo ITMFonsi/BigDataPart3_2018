137e4a32d9309e9b19e89bba7f9215801b3eedd2
fbe7236 <fbe7236@students.fhv.at>
2018-12-19 10:32:03 +0100

    int

--- maxtempmapreduce-bigdata/src/main/java/WeatherMapper.java
+++ maxtempmapreduce-bigdata/src/main/java/WeatherMapper.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
@@ -8,23 +9,23 @@ import org.apache.hadoop.mapred.Reporter;
 
 import java.io.IOException;
 
-public class WeatherMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
+public class WeatherMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
     private static final int EXCLUDED_TEMP = 9999;
     private static final String QUALITY_CODE = "[01459]";
 
-    public void map(LongWritable longWritable, Text text, OutputCollector<Text, DoubleWritable> outputCollector, Reporter reporter) throws IOException {
+    public void map(LongWritable longWritable, Text text, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
         String dataLine = text.toString();
         String dataYear = dataLine.substring(15, 19);
-        double temp ;
+        int temp ;
         if(dataLine.charAt(87) == '+') {
-            temp = Double.parseDouble(dataLine.substring(88, 92));
+            temp = Integer.parseInt(dataLine.substring(88, 92));
         } else {
-            temp = Double.parseDouble(dataLine.substring(87, 92));
+            temp = Integer.parseInt(dataLine.substring(87, 92));
         }
         String qual = dataLine.substring(92,93);
         if(temp != EXCLUDED_TEMP && qual.matches(QUALITY_CODE)) {
-            outputCollector.collect(new Text(dataYear), new DoubleWritable(temp));
+            outputCollector.collect(new Text(dataYear), new IntWritable(temp));
         }
     }
 }
--- maxtempmapreduce-bigdata/src/test/java/WeatherMapReduceTest.java
+++ maxtempmapreduce-bigdata/src/test/java/WeatherMapReduceTest.java
@@ -1,2 +1,43 @@
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class WeatherMapReduceTest {
+
+    MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+    ReduceDriver<Text, DoubleWritable, Text, IntWritable> reduceDriver;
+    MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+
+    @Before
+    public void setUp() {
+        WeatherMapper mapper = new WeatherMapper();
+        WeatherReducer reducer = new WeatherReducer();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+
+    }
+
+    @Test
+    public void testMapper() {
+
+    }
+
+    @Test
+    public void testReducer() {
+
+    }
+
+    @Test
+    public void testMapReduce() {
+
+    }
 }
+
+
